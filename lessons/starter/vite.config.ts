a4ae9ba9826c0e2825f126806b243252132513db
alvarosabu <alvaro.saburido@gmail.com>
2023-12-19 10:32:27 +0100

    feat: added color picker components

--- lessons/starter/vite.config.ts
+++ lessons/starter/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { templateCompilerOptions } from '@tresjs/core'
 import UnoCSS from 'unocss/vite'
+import { presetIcons, presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,17 @@ export default defineConfig({
     vue({
     ...templateCompilerOptions
     }),
-    UnoCSS()
+    UnoCSS({
+      presets: [
+        presetUno(),
+        presetIcons({
+          extraProperties: {
+            'display': 'inline-block',
+            'vertical-align': 'middle',
+            // ...
+          },
+        }),
+      ]
+    })
   ],
 })
