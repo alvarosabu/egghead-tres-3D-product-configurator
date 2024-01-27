5bc93fe354eab3ba548c8bb0f8cf039aa31d3f0d
alvarosabu <alvaro.saburido@gmail.com>
2024-01-27 13:11:09 +0100

    feat: add tres-canvas example for lessons 02-04

---
+++ lessons/tres-canvas/README.md
@@ -0,0 +1,6 @@
+# Lesson 02-04
+
+This code covers the following lessons:
+ - 02: Add a TresCanvas to your Vue app
+ - 03: Add a cube to your 3D Scene
+ - 04: Transform objects using props
---
+++ lessons/tres-canvas/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+
+</script>
+
+<template>
+  <TresCanvas window-size clear-color="#82DBC5">
+    <TresMesh 
+      :position="[0, 1, 0]" 
+      :scale="[2, 0.5, 1]" 
+      :rotation="[Math.PI / 4, -Math.PI / 3,0]"
+    >
+      <TresBoxGeometry :args="[1,1,1]"/>
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    <TresAxesHelper :args="[5]" />
+  </TresCanvas>
+</template>
