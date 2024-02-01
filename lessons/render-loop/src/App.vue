8c6b39a667d9a757f82463a3cb76841c4e0fb392
alvarosabu <alvaro.saburido@gmail.com>
2024-02-01 08:41:09 +0100

    feat: initial render-loop lesson

---
+++ lessons/render-loop/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <TresCanvas window-size clear-color="#82DBC5">
+    <TresPerspectiveCamera 
+      :position="[5,5,5]" :look-at="[0,0,0]"
+    />
+    <TresMesh :position="[0,2,0]">
+      <TresBoxGeometry :args="[1,1,1]"/>
+      <TresMeshNormalMaterial />
+    </TresMesh>
+    <TresAxesHelper :args="[5]" />
+  </TresCanvas>
+</template>
+
---
+++ lessons/render-loop/src/components/BaseColors.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+
+const selectedBaseColor = ref('#903345')
+const baseColors = reactive(['#903345', '#F2D3AC', '#F2F2F2', '#000000'])
+</script>
+
+<template>
+  <div class="mb-8">
+    <h3>Base Color</h3>
+    <ul class="flex gap-4 flex-wrap list-none justify-start ml-0 pl-0">
+      <li v-for="(color, index) in baseColors" :key="index" class="relative">
+        <span v-show="selectedBaseColor === color" 
+          class="absolute -top-2 -right-1 bg-gray-200 text-xs w-18px h-18px flex items-center justify-center rounded-full">
+          <i class="i-carbon-checkmark "  />
+        </span>
+        <button
+        :style="{ backgroundColor: color}"
+        @click="selectedBaseColor = color" 
+        class="w-8 h-8 rounded-full border-2 border-white border-solid cursor-pointer">
+         
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
