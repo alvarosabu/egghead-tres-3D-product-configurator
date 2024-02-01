89a07108d2e785729d0d9c413109f8c483c86a88
alvarosabu <alvaro.saburido@gmail.com>
2024-02-01 10:34:35 +0100

    feat: final light and shadows scene

--- lessons/light-shadows/src/App.vue
+++ lessons/light-shadows/src/App.vue
@@ -3,15 +3,35 @@ import { TresCanvas } from '@tresjs/core'
 </script>
 
 <template>
-  <TresCanvas window-size clear-color="#82DBC5">
+  <TresCanvas shadows window-size clear-color="#000">
     <TresPerspectiveCamera 
       :position="[5,5,5]" :look-at="[0,0,0]" 
     />
     <TresMesh 
-      :position="[0, 2, 0]"  
+      :position="[0, 2, 0]"
+      receive-shadow
+      cast-shadow 
     >
       <TresBoxGeometry/>
-      <TresMeshNormalMaterial />
+      <TresMeshStandardMaterial color="gray" />
+    </TresMesh>
+    <TresAmbientLight :args="['white', 2]" />
+    <TresDirectionalLight 
+      :position="[4,6,0]"
+      :color="'cyan'"
+      :intensity="1"
+      cast-shadow
+    />
+    <TresMesh 
+      :position="[2, 4, 0]" 
+      cast-shadow
+    >
+      <TresSphereGeometry/>
+      <TresMeshStandardMaterial color="pink" />
+    </TresMesh>
+    <TresMesh receive-shadow  :rotation-x="-Math.PI / 2">
+      <TresPlaneGeometry :args="[10,10]" />
+      <TresMeshStandardMaterial color="white" />
     </TresMesh>
     <TresAxesHelper :args="[5]" />
   </TresCanvas>
