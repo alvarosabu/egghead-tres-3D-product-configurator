caf11f71ea9b737b944f85b103f14787c83e3a9c
alvarosabu <alvaro.saburido@gmail.com>
2024-03-19 10:34:46 +0100

    feat: initial lesson 9-10

---
+++ lessons/model/README.md
@@ -0,0 +1,7 @@
+# Lesson 09-10
+
+This code covers the following lessons:
+ - 09: Quick overview of the template provided
+ - 10:  Add a 3D Model to a TresJS scene
+
+
---
+++ lessons/model/src/App.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+import { OrbitControls, ContactShadows } from '@tresjs/cientos';
+import TheHeader from './components/TheHeader.vue';
+import BaseColors from './components/BaseColors.vue';
+import CushionColors from './components/CushionColors.vue';
+</script>
+
+<template>
+  <TheHeader />
+  <main class="relative mx-auto container h-screen flex items-center">
+    <div class="w-full grid grid-cols-1 sm:grid-cols-2">
+      <section class="min-h-400px">
+        <TresCanvas alpha>
+          <TresPerspectiveCamera :position="[0,0,11]" />
+          <OrbitControls />
+   
+          <ContactShadows
+            :opacity="0.2"
+            :blur="3"
+            :position="[0, -2, 0]" 
+          />
+          <TresAmbientLight :args="['#fff', 1]" />
+          <TresDirectionalLight :intensity="2" :position="[4,4,4]" />
+        </TresCanvas>
+      </section>
+      <aside class="ml-16">
+        <h2 class="mb-16">
+          🥚-headphones
+        </h2>
+        <BaseColors />
+        <CushionColors />
+      </aside>
+    </div>
+  </main>
+</template>
